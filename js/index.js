abf99dca9b0451294b7b159ca0489c319186080a
msantoss16 <matheus.augusto2077@gmail.com>
2020-07-24 02:55:29 -0300

    :package: Formulario

--- js/index.js
+++ js/index.js
@@ -3,3 +3,17 @@ var hamburguer = document.querySelector("#hamburguer");
 hamburguer.addEventListener("click", function () {
   document.querySelector(".container").classList.toggle("show-menu");
 });
+
+changed = (event) => {
+  if (event.target.value !== "" && !event.target.classList.contains("notblank")) {
+    event.target.classList.toggle("notblank");
+  }
+}
+/* 
+var field = document.querySelector(".field");
+
+field.addEventListener('change', function () {
+  if (field.value !== "" && !field.classList.contains("notblank")) {
+    field.classList.toggle("notblank");
+  }
+}); */
