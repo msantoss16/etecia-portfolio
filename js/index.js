e211066532b220d165952fe7d88ed8e187b1e2cc
msantoss16 <matheus.augusto2077@gmail.com>
2020-07-28 21:09:59 -0300

    :up: Atualização do Orçamento (JS)

--- js/index.js
+++ js/index.js
@@ -21,3 +21,24 @@ fOrcamento = (event) => {
     '_blank'
   );
 }
+
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value;
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`;
+    atualizarPreco();
+})
+
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").value;
+    const temJS = document.querySelector("#js").checked;
+    const incluiLayout = document.querySelector("#layout-sim").checked;
+    const prazo = document.querySelector("#prazo").value;
+  
+    let preco = qtde * 100;
+    if (temJS) preco *= 1.1
+    if (incluiLayout) preco += 500
+    let taxaUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaUrgencia
+   
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
